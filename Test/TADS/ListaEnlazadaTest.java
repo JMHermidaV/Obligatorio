9755390a4cc4480a635fa5de6656f64783bda380
Juan Manuel <manuhermida8@gmail.com>
2021-06-05 11:33:12 -0300

    "Cambio el TAD linkedList y le agrego un test"

--- Test/TADS/ListaEnlazadaTest.java
+++ Test/TADS/ListaEnlazadaTest.java
@@ -1,4 +1,35 @@
-import junit.framework.TestCase;
-public class ListaEnlazadaTest extends TestCase {
-  
+package TADS;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ListaEnlazadaTest {
+
+    @Test
+    public void testFlujoCompleto() {
+        ListaEnlazada<Integer> MiLista = new ListaEnlazada();
+
+        assertEquals(0,MiLista.size());
+        MiLista.add(4);
+        assertEquals(1,MiLista.size());
+        MiLista.add(23);
+        assertEquals(2,MiLista.size());
+        MiLista.add(1);
+        assertEquals(3,MiLista.size());
+        MiLista.add(5);
+        assertEquals(4,MiLista.size());
+
+        assertTrue(MiLista.contains(4));
+        assertTrue(MiLista.contains(5));
+        assertTrue(MiLista.contains(23));
+        assertFalse(MiLista.contains(7));
+
+        assertEquals(23, MiLista.get(2));
+        assertEquals(4, MiLista.get(1));
+
+        MiLista.remove(2);
+        assertEquals(3,MiLista.size());
+        assertEquals(1, MiLista.get(2));
+        assertFalse(MiLista.contains(23));
+    }
 }
